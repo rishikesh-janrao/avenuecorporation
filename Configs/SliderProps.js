a292aa6921f158179a7598d59b20010997ce61b4
janraor <rishikesh.janrao@dominos.com>
2022-10-23 21:49:54 +0530

    Slider styles

--- Configs/SliderProps.js
+++ Configs/SliderProps.js
@@ -48,7 +48,7 @@ const SliderProps = {
       coverflowEffect: {
         rotate: 50,
         stretch: 0,
-        depth: 100,
+        depth: 10,
         modifier: 1,
         slideShadows: true,
       },
