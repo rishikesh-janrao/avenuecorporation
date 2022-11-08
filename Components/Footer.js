53ccbe521153c42a03c06efece4ee1e187f0d26c
rishikesh-janrao <janraorishikesh@gmail.com>
2022-11-08 10:00:20 +0530

    adds footerLinks and meetour clients section to footer component, updates favicon

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,14 +1,21 @@
+import { useRouter } from 'next/router';
 import footerStyles from '../styles/Footer.module.css'
+import Sections from './Sections';
 
 
 function Footer(props) {
+  const router = useRouter();
   return (
+    <>
+    {router.route != '/' && (<Sections.MeetOurClients showClientSlider={false}/>)}
+    <Sections.FooterLinks />
     <footer className={footerStyles.footer}>
       <a>
         <span>© Copyright 2022 Avenue Corporation.&nbsp;</span> 
         <span>All rights reserved.</span>
       </a>
     </footer>
+    </>
   );
 }
 
