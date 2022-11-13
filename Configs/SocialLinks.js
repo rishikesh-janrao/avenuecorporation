d06cb85f7c691e0e952212fdcbb6180829ccce31
janraor <rishikesh.janrao@dominos.com>
2022-11-13 14:30:51 +0530

    updates whatsapp mobile number

--- Configs/SocialLinks.js
+++ Configs/SocialLinks.js
@@ -3,7 +3,7 @@ const SocialLinks = {
   instagram: '/instagram',
   twitter: '/twitter',
   whatsapp:
-    'https://api.whatsapp.com/send?phone=919730272570&text=Hi, I need more info on cost-effective solutions for sustainable and export packaging',
+    'https://api.whatsapp.com/send?phone=918956747731&text=Hi, I need more info on cost-effective solutions for sustainable and export packaging',
   youtube: '/youtube',
   linkedin: '/linkedIn',
 };
