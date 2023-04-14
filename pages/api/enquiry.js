bed3129fbc623ae6dde13ae4e290c225a0aa2e00
Rishikesh Janrao <65944832+rishikesh-janrao@users.noreply.github.com>
2023-04-14 17:36:56 +0530

    updates for GET method to enquiry

--- pages/api/enquiry.js
+++ pages/api/enquiry.js
@@ -4,27 +4,25 @@ const { insertEnquiry, getAllEnquiries } = FBServices();
 export default function handler(req, res) {
   const { action } = req.query;
   const parsedAction = atob(action);
-  if (req.method === "POST") {
-    if (req.body) {
-      const { timestamp: t, clientData } = req.body;
+  if (req.body) {
+    const { timestamp: t, clientData } = req.body;
 
-      switch (parsedAction) {
-        case "ADD":
-          //POST CALL
-          const insertCompleted = (response) => {
-            res.status(200).json({ status: "tracked", ...response });
-          };
-          insertEnquiry(clientData, insertCompleted);
-          break;
-        case "GETALL":
-          function response(data){
-            if(data){
-              res.status(200).json(data);
-            }
+    switch (parsedAction) {
+      case "ADD":
+        //POST CALL
+        const insertCompleted = (response) => {
+          res.status(200).json({ status: "tracked", ...response });
+        };
+        insertEnquiry(clientData, insertCompleted);
+        break;
+      case "GETALL":
+        function response(data) {
+          if (data) {
+            res.status(200).json(data);
           }
-          getAllEnquiries(response)
-          break;
-      }
+        }
+        getAllEnquiries(response);
+        break;
     }
   }
 }
